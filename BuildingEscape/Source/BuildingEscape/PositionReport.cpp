2cabc3049e7b2dfe960578a39f4317448dcfa04f
Jeff Largent <bass.warrior@gmail.com>
2016-09-01 14:29:21 -0500

    End of 57

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+ // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
 #include "PositionReport.h"
@@ -21,7 +21,7 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on chair!"));
 	
 }
 
